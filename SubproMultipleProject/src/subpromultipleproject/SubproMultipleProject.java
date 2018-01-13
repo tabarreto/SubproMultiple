3d053ef93992ce66933478ec339487bb5279ac1b
antonio <antonio@EQUIPO-TONY>
2018-01-13 08:05:55 -0500

    Multitarea en java

--- SubproMultipleProject/src/subpromultipleproject/SubproMultipleProject.java
+++ SubproMultipleProject/src/subpromultipleproject/SubproMultipleProject.java
@@ -15,7 +15,7 @@ public class SubproMultipleProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Primer commit");
     }
     
 }
