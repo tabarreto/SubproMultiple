bb7399a58259be0af3507902b804fd4f42039651
Sandra <Sandra@10.8.66.244>
2018-01-17 19:02:06 -0600

    Merge origin/master
    Conflicts:
    	SubproMultipleProject/src/subpromultipleproject/SubproMultipleProject.java

--- SubproMultipleProject/src/subpromultipleproject/SubproMultipleProject.java
+++ SubproMultipleProject/src/subpromultipleproject/SubproMultipleProject.java
@@ -15,8 +15,7 @@ public class SubproMultipleProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Primer commit");
-        //Todo fue borrado
+        System.out.println("Desde el apto");
     }
 
 }
